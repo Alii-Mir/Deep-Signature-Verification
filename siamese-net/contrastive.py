ea372547d9b65e86a506a9723b002fbc456da645
Ali Mir <mohammadalimir110@gmail.com>
2025-03-20 18:25:12 -0230

    Update model based on FCL classifier

--- siamese-net/contrastive.py
+++ siamese-net/contrastive.py
@@ -6,18 +6,22 @@ import torch.nn.functional as F
 class ContrastiveLoss(nn.Module):
     "Contrastive loss function"
 
-    def __init__(self, margin=0.5):   #margin=2.0
+    def __init__(self, margin=0.1):   # margin=2.0, 0.5, 0.1, 0.05, 0.01
         super(ContrastiveLoss, self).__init__()
         self.margin = margin
 
     def forward(self, output1, output2, label):
+
+        # euclidean distance
         euclidean_distance = F.pairwise_distance(output1, output2)
-        loss_contrastive = torch.mean(
-            (1 - label) * torch.pow(euclidean_distance, 2)
-            + (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2)
-        )
+
+        genuine_loss = (1 - label) * torch.pow(euclidean_distance, 2)
+        forged_loss = (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2)
+        separation_loss = 0   # (label) * torch.pow(2*self.margin - euclidean_distance, 2)  #Distance Loss Term
+
+        loss_contrastive = torch.mean(genuine_loss + forged_loss + separation_loss)
 
         return loss_contrastive
-    
+
 # For genuine pairs (label = 0), the loss is euclidean_distance².
 # For forged pairs (label = 1), the loss is (max(0, margin - euclidean_distance))².
